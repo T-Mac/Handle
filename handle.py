626d95949f657a7dd2d3062a37fd91aca615bd61
Taylor McKinnon <htmlfreak117@gmail.com>
2012-03-11 16:01:12 -0600

    added base prog class

--- handle.py
+++ handle.py
@@ -2,38 +2,27 @@ import threading
 import Queue
 import lib.server as server
 
-class Handle(threading.Thread):
+class Handle(Base):
 	def __init__(self):
-		self.tasks = Queue.Queue(maxsize=0)
-		self.comp{}
-		self.exit = False
-		#create Database instance
-		self.comp['database'] = server.Database()
-		#load config into database
-		self.comp['database'].loadconfig()
+		Base.__init__( self )
 		#create instance of bukkit controller
 		self.comp['bukkit'] = server.Bukkit(self.comp['database'])
 		#create Networking
 		self.comp['network'] = network.Network('server', self.tasks, self.comp['database'].config['Handle']['port'])
-		
-		threading.Thread.__init__( self )
-		
-	def run(self):
-		while not self.exit:
-			task = self.tasks.get(True,5)
-			self.parsetask(task)			
-		
-		
-	def addtask(self, task)
-		self.tasks.put(task)
 	
 	def parsetask(self, task)
 		if task['id'] == 'network.lineup':
 			self.comp['network'].send({'id':'clientup', 'item':'lineup', 'data':task['data']})
 		if task['id'] == 'handle.command':
 			self.parsecommand(task['data'])
+		if task['id'] == 'server.start':
+			self.comp['bukkit'].startserver()
+		if task['id'] == 'server.stop':
+			self.comp['bukkit'].stopserver()
+			
 	def start(self):
 		self.comp['network'].start()
+		Base.start( self )
 
 	def parsecommand(self, command):
 		if command == 'start':
@@ -50,3 +39,30 @@ class Client(threading.Thread):
 			self.network = netork.Network('client', self.tasks, self.data.config['Handle']['port'])
 
 			self.gui = 
+
+class Base(threading.Thread):
+	def __init__(self):
+		self.comp = {}
+		self.tasks = Queue.Queue(maxsize = 0)
+		self.exit = False
+		self.comp['database'] = server.Database()
+		self.comp['database'].loadconfig()
+		
+		threading.Thread.__init__( self )
+	
+	def run(self):
+		while not self.exit:
+			task = self.tasks.get(True,5)
+			self.parsetask(task)
+
+	def addtask(self, task)
+		self.tasks.put(task)
+		
+	def parsetask(self, task)
+		return False
+	
+	
+	
+	
+	
+	
